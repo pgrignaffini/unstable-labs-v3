f6670bca25c5756019e6b86a1475b27562abc2f6
grignaffini <grignaffinipaolo@gmail.com>
2022-11-13 17:34:50 +0100

    first commit

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,10 +1,8 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-
-import { trpc } from "../utils/trpc";
-
-import "../styles/globals.css";
+import { trpc } from "@utils/trpc";
+import "@styles/globals.css";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
