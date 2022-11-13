cbf6ef9b857559fe7713e57e3c03971671bdc405
grignaffini <grignaffinipaolo@gmail.com>
2022-11-13 17:26:59 +0100

    Revert "first commit"
    This reverts commit 8267efd921d38db11af1aa7cc80cb99572b3e0c8.

--- frontend/environment.d.ts
+++ frontend/environment.d.ts
@@ -1,10 +1,6 @@
 declare namespace NodeJS {
     interface ProcessEnv {
-        ALCHEMY_API_KEY: string;
-        NEXT_PUBLIC_PINATA_KEY: string;
-        NEXT_PUBLIC_PINATA_SECRET: string;
-        NEXT_PUBLIC_PINATA_JWT: string;
-        NEXT_PUBLIC_STABLE_HORDE_API_KEY: string;
-        NEXT_PUBLIC_PRIVATE_KEY: string;
+        // PRIVATE_KEY: string;
+        // ALCHEMY_URL: string;
     }
 }
