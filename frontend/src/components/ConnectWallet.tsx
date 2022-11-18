8676171fb7c45b4af49d531c8c22d8613c8b439d
grignaffini <grignaffinipaolo@gmail.com>
2022-11-18 12:05:19 +0100

    added automatic switch network

--- frontend/src/components/ConnectWallet.tsx
+++ frontend/src/components/ConnectWallet.tsx
@@ -1,14 +1,25 @@
-import { useAccount, useConnect, useDisconnect } from 'wagmi'
+import { useEffect } from 'react'
+import { useAccount, useConnect, useDisconnect, useSwitchNetwork, useNetwork } from 'wagmi'
 import { InjectedConnector } from 'wagmi/connectors/injected'
 import makeBlockie from "ethereum-blockies-base64"
 
 function ConnectWallet() {
+    const chainId = 1313161555
     const { address, isConnected } = useAccount()
+    const { chain } = useNetwork()
+    const { switchNetwork } = useSwitchNetwork()
+    const isMismatched = chainId !== chain?.id
     const { connect } = useConnect({
         connector: new InjectedConnector(),
     })
     const { disconnect } = useDisconnect()
     const displayAddress = address ? `${address.slice(0, 6)}...${address.slice(-4)}` : ''
+
+    useEffect(() => {
+        if (isMismatched) {
+            switchNetwork?.(chainId);
+        }
+    }, [address]);
 
     return (
         <>
