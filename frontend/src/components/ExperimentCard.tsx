17f005b213bb72c22c13165b6cadfbdfbf300391
grignaffini <grignaffinipaolo@gmail.com>
2022-11-24 16:50:44 +0100

    implementing responsiveness

--- frontend/src/components/ExperimentCard.tsx
+++ frontend/src/components/ExperimentCard.tsx
@@ -14,12 +14,12 @@ function ExperimentCard({ experiment }: Props) {
         <>
             <input type="checkbox" id={experiment.name} className="modal-toggle" />
             <div className="modal h-full w-full">
-                <div className="flex flex-col space-y-3">
-                    <label htmlFor={experiment.name} className="text-2xl text-white cursor-pointer">X</label>
+                <div className="flex flex-col space-y-3  text-white">
+                    <label htmlFor={experiment.name} className="text-2xl cursor-pointer">X</label>
                     <img src={src} alt={experiment?.name} className="w-full" />
-                    <div className="flex flex-col items-center justify-center">
+                    <div className="flex flex-col items-center justify-center bg-black" >
                         <h1 className="text-sm lg:text-lg 2xl:text-2xl font-bold">{experiment?.name}</h1>
-                        {experiment?.description && <p className="text-sm lg:text-md 2xl:text-lg italic">&quot;{experiment.description}&quot;</p>}
+                        {experiment?.description && <p className="text-[0.6rem] lg:text-md 2xl:text-lg italic">&quot;{experiment.description}&quot;</p>}
                     </div>
                 </div>
             </div>
@@ -29,19 +29,17 @@ function ExperimentCard({ experiment }: Props) {
     return (
         <>
             {showExperimentModal}
-            <div className='border-2 hover:border-4  hover:border-acid hover:-m-1 p-4'>
-                <div className="flex flex-col">
-                    <label htmlFor={experiment.name} className='cursor-pointer space-y-2'>
-                        <div className='w-auto h-32 md:h-48 2xl:h-60 relative min-w-1/5'>
-                            <Image src={src} alt={experiment?.name} fill objectFit='cover' />
-                        </div>
-                        <div className='flex items-center justify-between'>
-                            <div className="text-white text-sm lg:text-lg 2xl:text-xl font-bold">{experiment?.name}</div>
-                            {experiment?.tokenId && <LikeButton tokenId={experiment.tokenId} />}
-                        </div>
-                    </label>
+            <label htmlFor={experiment.name} className='cursor-pointer space-y-2'>
+                <div className='h-72 lg:h-64 xl:h-72 2xl:h-96 w-auto flex flex-col justify-between items-center border-2 hover:border-4 hover:border-acid hover:-m-1 p-4'>
+                    <div className='relative w-full h-full'>
+                        <Image src={src} alt={experiment?.name} placeholder='blur' blurDataURL='/blur.jpeg' fill sizes='100vw' style={{ objectFit: 'contain' }} />
+                    </div>
+                    <div className='flex items-center'>
+                        <div className="text-white text-[0.6rem] lg:text-sm 2xl:text-md font-bold">{experiment?.name}</div>
+                        {experiment?.tokenId && <LikeButton tokenId={experiment.tokenId} />}
+                    </div>
                 </div>
-            </div>
+            </label>
         </>
     )
 }
