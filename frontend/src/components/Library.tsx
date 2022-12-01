737221aeed397a674d75c5c41e8582723f8dce17
grignaffini <grignaffinipaolo@gmail.com>
2022-12-01 18:25:07 +0100

    implementing library

--- frontend/src/components/Library.tsx
+++ frontend/src/components/Library.tsx
@@ -9,7 +9,7 @@ function Library({ }: Props) {
     const { allPapers } = usePapers()
 
     return (
-        <div className="col-span-full h-screen overflow-y-scroll space-y-5">
+        <div className="col-start-2 col-end-5 h-screen overflow-y-scroll space-y-5">
             {allPapers?.map(paper => (
                 <Paper paper={paper} key={paper.id} />
             ))}
